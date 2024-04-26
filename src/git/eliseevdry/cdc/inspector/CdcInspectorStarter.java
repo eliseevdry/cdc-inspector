53b1e07952921e0197c623ca49ccc4b1799f4e3a
Andrey Eliseev <andrey.s.eliseev@lenta.com>
2024-04-26 16:01:37 +0300

    Initial commit

---
+++ src/git/eliseevdry/cdc/inspector/CdcInspectorStarter.java
@@ -0,0 +1,7 @@
+package git.eliseevdry.cdc.inspector;
+
+public class CdcInspectorStarter {
+    public static void main(String[] args) {
+        System.out.println("Stub for CDC Inspector");
+    }
+}
